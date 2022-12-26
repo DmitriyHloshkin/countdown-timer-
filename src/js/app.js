62fa983deb87afe9893488437fac2833a2c6401e
Dmitriy Hloshkin <dmitriyhloshkin@gmail.com>
2022-12-26 22:30:08 +0200

    new desine, adding header and header bar

--- src/js/app.js
+++ src/js/app.js
@@ -3,17 +3,17 @@ import timer from './modules/counter.js';
 
 document.addEventListener("DOMContentLoaded", () => {
   checkWebp();
-  timer({
-    selectorDays: '#days',
-    selectorHours: '#hours',
-    selectorMinutes: '#minutes',
-    selectorSeconds: '#seconds',
-    timeToStop: new Date(new Date().setHours(23, 59, 59, 999)),
-    circle: {
-      circleSeconds: '.circle__segment-seconds',
-      circleMinutes: '.circle__segment-minutes',
-      circleHours: '.circle__segment-hours',
-      circleDays: '.circle__segment-days',
-    },
-  });
+  // timer({
+  //   selectorDays: '#days',
+  //   selectorHours: '#hours',
+  //   selectorMinutes: '#minutes',
+  //   selectorSeconds: '#seconds',
+  //   timeToStop: new Date(new Date().setHours(23, 59, 59, 999)),
+  //   circle: {
+  //     circleSeconds: '.circle__segment-seconds',
+  //     circleMinutes: '.circle__segment-minutes',
+  //     circleHours: '.circle__segment-hours',
+  //     circleDays: '.circle__segment-days',
+  //   },
+  // });
 });
